4e58ac1e429a56f163a27326046cd150d3cb56f9
saharty7 <ahmedamrelsaharty@gmail.com>
2026-09-09 12:38:51 +0200

    Added Some Guest Attributes

--- HotelBookingPlatform/Models/Guest.cs
+++ HotelBookingPlatform/Models/Guest.cs
@@ -6,4 +6,7 @@ public class Guest
     public string Name { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public string Phone { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+    public int Age { get; set; }
+    public string Gender { get; set; } = string.Empty;
 }
